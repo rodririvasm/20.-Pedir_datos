bbd0da79d04b7a18579c0301259bd7183b330ca0
rodrigo <rodrigorivasmiranda@gmail.com>
2021-07-15 17:22:21 -0500

    actualizado

---
+++ src/PedirDatos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PedirDatos {
+	public static void main(String[] args) {
+		
+		
+		Scanner Entrada=new Scanner(System.in);
+		System.out.println("ingres tu nombre:");
+		String nombre=Entrada.nextLine();
+		System.out.println("hola "+nombre);
+				
+	}
+}
+
